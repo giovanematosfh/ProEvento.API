811862a8e35a02feb6175caa53d74a6a1ca88d53
Giovane Matos <filho1987@gmail.com>
2022-11-25 18:18:25 -0300

    Dotnet 5

---
+++ Back/Models/TarefasContexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDeTarefas.Models
+{
+    public class TarefasContexto : DbContext
+    {
+        public DbSet <Tarefas> Tarefas { get; set; }
+        public TarefasContexto(DbContextOptions<TarefasContexto> options) : base(options)
+        {
+
+        }
+    }
+}
